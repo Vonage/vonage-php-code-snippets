d918638aa75e7293db0c3b377cc1a22831179fc3
leggetter <phil@leggetter.co.uk>
2017-06-09 17:38:55 +0100

    TTS outbound - variable updates

--- voice/text-to-speech-outbound.php
+++ voice/text-to-speech-outbound.php
@@ -1,21 +1,24 @@
 <?php 
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once 'vendor/autoload.php';
+// Building Blocks
+// 1. Make a Phone Call
+// 2. Play Text-to-Speech
 
-$basic  = new \Nexmo\Client\Credentials\Basic(API_KEY, API_SECRET);
-$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(PRIVATE_KEY), APPLICATION_ID);
+$basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
+$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_PRIVATE_KEY), NEXMO_APPLICATION_ID);
 
 $client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Container($basic, $keypair));
 
 $call = $client->calls()->create([
     'to' => [[
         'type' => 'phone',
-        'number' => TO_NUMBER
+        'number' => NEXMO_TO_NUMBER
     ]],
     'from' => [
         'type' => 'phone',
-        'number' => FROM_NUMBER
+        'number' => NEXMO_FROM_NUMBER
     ],
     'answer_url' => ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json'],
 ]);
