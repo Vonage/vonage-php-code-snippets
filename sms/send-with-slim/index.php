d969e243bb5a11b0ee5296bf50ba7b40212aa215
Michael Heap <m@michaelheap.com>
2017-09-12 17:45:35 +0100

    Add send SMS with SlimPHP example

---
+++ sms/send-with-slim/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+$app = new Slim\App();
+
+$app->post('/sms/{number}', function ($request, $response, $args) {
+    $body = $request->getParsedBody();
+
+    if (!isset($body['text'])) {
+        return $response->withStatus(400)->write("No message provided");
+    }
+
+    $client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(API_KEY, API_SECRET));
+    $text = new \Nexmo\Message\Text($args['number'], NEXMO_FROM_NUMBER, $body['text']);
+    $client->message()->send($text);
+    return $response->write("Sending an SMS to " . $args['number']);
+});
+
+$app->run();
