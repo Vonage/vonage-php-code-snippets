9fc2a1b0528badd2508ee6f0e81f6f77fc0e71e8
Michael Heap <m@michaelheap.com>
2017-10-26 15:03:31 +0100

    Update text-to-speech-outbound example with new NDP standards

--- voice/text-to-speech-outbound.php
+++ voice/text-to-speech-outbound.php
@@ -6,21 +6,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // 1. Make a Phone Call
 // 2. Play Text-to-Speech
 
-$basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
-$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_PRIVATE_KEY), NEXMO_APPLICATION_ID);
-
-$client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Container($basic, $keypair));
+$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY), NEXMO_APPLICATION_ID);
+$client = new \Nexmo\Client($keypair);
 
 $call = $client->calls()->create([
     'to' => [[
         'type' => 'phone',
-        'number' => NEXMO_TO_NUMBER
+        'number' => TO_NUMBER
     ]],
     'from' => [
         'type' => 'phone',
-        'number' => NEXMO_FROM_NUMBER
+        'number' => NEXMO_NUMBER
     ],
-    'answer_url' => ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json'],
+    'answer_url' => ['https://developer.nexmo.com/ncco/tts.json'],
 ]);
 
 error_log($call);
