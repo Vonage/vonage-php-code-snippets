4385513a6f80682ad865f6a04eda795af237822e
Michael Heap <m@michaelheap.com>
2017-11-29 12:44:24 +0000

    Fix inbound call example (GET only)

--- voice/inbound-calls-slim/index.php
+++ voice/inbound-calls-slim/index.php
@@ -5,20 +5,8 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->any('/webhooks/answer', function (Request $request, Response $response) {
-    // Nexmo may send a GET or a POST request, depending on your account
-    // settings. Reject anything that isn't a GET/POST
-    if (!in_array($request->getMethod(), ['GET', 'POST'])) {
-        return $response->withStatus(404);
-    }
-
-    $params = $request->getParsedBody();
-
-    // Fall back to reading the query string if the body is empty
-    if (!isset($params['from'])) {
-        $params = $request->getQueryParams();
-    }
-
+$app->get('/webhooks/answer', function (Request $request, Response $response) {
+    $params = $request->getQueryParams();
     $fromSplitIntoCharacters = implode(" ", str_split($params['from']));
 
     $ncco = [
