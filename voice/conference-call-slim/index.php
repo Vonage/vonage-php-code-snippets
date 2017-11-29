485f1a3b90efc4c5a7d7cd196a4bd80aa4328a39
Michael Heap <m@michaelheap.com>
2017-11-29 12:57:34 +0000

    Updated conference call example

--- voice/conference-call-slim/index.php
+++ voice/conference-call-slim/index.php
@@ -6,13 +6,7 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->any('/webhooks/answer', function (Request $request, Response $response) {
-    // Nexmo may send a GET or a POST request, depending on your account
-    // settings. Reject anything that isn't a GET/POST
-    if (!in_array($request->getMethod(), ['GET', 'POST'])) {
-        return $response->withStatus(404);
-    }
-
+$app->get('/webhooks/answer', function (Request $request, Response $response) {
     $ncco = [
         [
             'action' => 'talk',
