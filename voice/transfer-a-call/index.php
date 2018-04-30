e5b61a7627b9ab8aeebb00bec541bd45580279a8
Michael Heap <m@michaelheap.com>
2018-04-30 17:07:41 +0100

    Add transfer example

---
+++ voice/transfer-a-call/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
+$client = new \Nexmo\Client($keypair);
+
+$client->calls[NEXMO_CALL_UUID]->put(new \Nexmo\Call\Transfer("https://developer.nexmo.com/ncco/transfer.json"));
