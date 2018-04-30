7ef77e53757895e4fc2d53b034d3ea70522e8619
Michael Heap <m@michaelheap.com>
2018-04-30 19:25:22 +0100

    Add stream example

---
+++ voice/play-audio-stream-in-to-call/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
+$client = new \Nexmo\Client($keypair);
+
+$stream = $client->calls[NEXMO_CALL_UUID]->stream();
+$stream->setUrl('https://nexmo-community.github.io/ncco-examples/assets/voice_api_audio_streaming.mp3');
+$stream->put();
