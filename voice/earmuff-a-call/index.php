7ad2b88b3a4fb59dc6a08d05fed3cd4176577b8f
Michael Heap <m@michaelheap.com>
2018-04-30 16:28:23 +0100

    Add Earmuff example

---
+++ voice/earmuff-a-call/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
+$client = new \Nexmo\Client($keypair);
+
+$client->calls[NEXMO_CALL_UUID]->put(new \Nexmo\Call\Earmuff());
+sleep(3);
+$client->calls[NEXMO_CALL_UUID]->put(new \Nexmo\Call\Unearmuff());
