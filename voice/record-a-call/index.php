7c606f7b75552f1fdeacd3888d10359f8eec9bb9
Michael Heap <m@michaelheap.com>
2018-05-01 12:59:53 +0100

    Add record-a-call

---
+++ voice/record-a-call/index.php
@@ -0,0 +1,40 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/webhooks/answer', function (Request $request, Response $response) {
+    $uri = $request->getUri();
+    $ncco = [
+        [
+            'action' => 'record',
+            'eventUrl' => [
+                $uri->getScheme().'://'.$uri->getHost().':'.$uri->getPort().'/webhooks/recording'
+            ]
+        ],
+        [
+            'action' => 'connect',
+            'from' => NEXMO_NUMBER,
+            'endpoint' => [
+                [
+                    'type' => 'phone',
+                    'number' => TO_NUMBER
+                ]
+            ]
+        ],
+
+    ];
+
+    return $response->withJson($ncco);
+});
+
+$app->post('/webhooks/recording', function (Request $request, Response $response) {
+    $params = $request->getParsedBody();
+    error_log($params['recording_url']);
+    return $response->withStatus(204);
+});
+
+$app->run();
