d31f7790c0142321c8d26b1076a2d0b2b3bac4fc
Lorna Jane Mitchell <lorna@lornajane.net>
2018-07-18 16:18:28 +0100

    Update the Slim example to use standard placeholders, also use main config

--- sms/send-with-slim/index.php
+++ sms/send-with-slim/index.php
@@ -1,5 +1,6 @@
 <?php
 
+require __DIR__ . '/../../config.php';
 require 'vendor/autoload.php';
 $app = new Slim\App();
 
@@ -10,8 +11,8 @@ $app->post('/sms/{number}', function ($request, $response, $args) {
         return $response->withStatus(400)->write("No message provided");
     }
 
-    $client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(API_KEY, API_SECRET));
-    $text = new \Nexmo\Message\Text($args['number'], NEXMO_FROM_NUMBER, $body['text']);
+    $client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET));
+    $text = new \Nexmo\Message\Text($args['number'], FROM, $body['text']);
     $client->message()->send($text);
     return $response->write("Sending an SMS to " . $args['number']);
 });
