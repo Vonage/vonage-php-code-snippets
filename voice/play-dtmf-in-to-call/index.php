2c0c377c3bc42dfff817522e9f7f36cebd3c066d
Lorna Jane Mitchell <lorna@lornajane.net>
2018-07-19 12:08:08 +0100

    Bring placeholders inline with building block content
    This change makes the PHP code hold the same placeholder variables as
    are described in the building block examples, and match the building
    block spec documents.

--- voice/play-dtmf-in-to-call/index.php
+++ voice/play-dtmf-in-to-call/index.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
 $client = new \Nexmo\Client($keypair);
 
-$dtmf = $client->calls[NEXMO_CALL_UUID]->dtmf();
+$dtmf = $client->calls[UUID]->dtmf();
 $dtmf->setDigits('2468#');
 $dtmf->put();
