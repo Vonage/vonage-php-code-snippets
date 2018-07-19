bc9444312ef0eb2c470d97123b7864d41f090251
Lorna Jane Mitchell <lorna@lornajane.net>
2018-07-19 12:08:08 +0100

    Bring placeholders inline with building block content
    This change makes the PHP code hold the same placeholder variables as
    are described in the building block examples, and match the building
    block spec documents.

--- voice/mute-a-call/index.php
+++ voice/mute-a-call/index.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
 $client = new \Nexmo\Client($keypair);
 
-$client->calls[NEXMO_CALL_UUID]->put(new \Nexmo\Call\Mute());
+$client->calls[UUID]->put(new \Nexmo\Call\Mute());
 sleep(3);
-$client->calls[NEXMO_CALL_UUID]->put(new \Nexmo\Call\Unmute());
+$client->calls[UUID]->put(new \Nexmo\Call\Unmute());
--- voice/play-audio-stream-in-to-call/index.php
+++ voice/play-audio-stream-in-to-call/index.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $keypair = new \Nexmo\Client\Credentials\Keypair(file_get_contents(NEXMO_APPLICATION_PRIVATE_KEY_PATH), NEXMO_APPLICATION_ID);
 $client = new \Nexmo\Client($keypair);
 
-$stream = $client->calls[NEXMO_CALL_UUID]->stream();
+$stream = $client->calls[UUID]->stream();
 $stream->setUrl('https://nexmo-community.github.io/ncco-examples/assets/voice_api_audio_streaming.mp3');
 $stream->put();
