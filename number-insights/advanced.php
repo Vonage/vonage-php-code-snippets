f37372fc2054969d12bdc999fe8c872ec4bb8728
Mark Lewin <mark.lewin@vonage.com>
2018-10-02 10:34:41 +0100

    Change number insight replaceable variable names

--- number-insights/advanced.php
+++ number-insights/advanced.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client($basic);
 
-$insights = $client->insights()->advanced(NEXMO_TO_NUMBER);
+$insights = $client->insights()->advanced(INSIGHT_NUMBER);
 
 switch($insights->getReachable()) {
     case 'reachable':
