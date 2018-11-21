8552bef1a3b5fa2d274e9bb31ee8c541e3899dee
Mark Lewin <mark.lewin@vonage.com>
2018-11-21 10:57:02 +0000

    Tidy existing verify building blocks

--- verify/cancel.php
+++ verify/cancel.php
@@ -3,13 +3,18 @@ require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Support direct execution from command line or via a GET request as a web server
-define('VERIFY_REQUEST_ID', isset($argv[1])? $argv[1]: $_GET['request_id']);
+define('REQUEST_ID', isset($argv[1])? $argv[1]: $_GET['request_id']);
 
-error_log('Cancelling request ' . VERIFY_REQUEST_ID);
+error_log('Cancelling `request_id` ' . REQUEST_ID);
 
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Container($basic));
 
-$result = $client->verify()->cancel(VERIFY_REQUEST_ID);
+try {
+    $result = $client->verify()->cancel(REQUEST_ID);
+    var_dump($result->getResponseData());
+}
 
-var_dump($result->getResponseData());
+catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+}
