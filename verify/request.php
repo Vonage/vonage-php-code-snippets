eaabb8a66bcc5ddbe23a1f2e8df696e870040474
Mark Lewin <mark.lewin@vonage.com>
2018-11-21 15:55:12 +0000

    Change verify NEXMO_TO_NUMBER to RECIPIENT_NUMBER

--- verify/request.php
+++ verify/request.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Container($basic));
 
-$verification = new \Nexmo\Verify\Verification(NEXMO_TO_NUMBER, 'Acme Inc');
+$verification = new \Nexmo\Verify\Verification(RECIPIENT_NUMBER, 'Acme Inc');
 $client->verify()->start($verification);
 
 echo "Started verification, `request_id` is " . $verification->getRequestId();
