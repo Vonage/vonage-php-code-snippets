31cf9728fe753bc4b3ba094f06e03a6ccc688ecc
Lorna Jane Mitchell <github@lornajane.net>
2019-03-19 11:04:02 +0000

    Update the send SMS block to match new spec (#29)

--- sms/send-sms.php
+++ sms/send-sms.php
@@ -5,10 +5,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client($basic);
 
-$message = $client->message()->send([
-    'to' => TO_NUMBER,
-    'from' => 'Acme Inc',
-    'text' => 'A text message sent using the Nexmo SMS API'
-]);
+try {
+    $message = $client->message()->send([
+        'to' => TO_NUMBER,
+        'from' => 'Acme Inc',
+        'text' => 'A text message sent using the Nexmo SMS API'
+    ]);
+    $response = $message->getResponseData();
 
-var_dump($message->getResponseData());
+    if($response['messages'][0]['status'] == 0) {
+        echo "The message was sent successfully\n";
+    } else {
+        echo "The message failed with status: " . $response['messages'][0]['status'] . "\n";
+    }
+} catch (Exception $e) {
+    echo "The message was not sent. Error: " . $e->getMessage() . "\n";
+}
+
