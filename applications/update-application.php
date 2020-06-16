acd64a880119644262970303194860967b610bdc
Chris Tankersley <chris@ctankersley.com>
2020-06-16 11:20:41 -0400

    Updated to v2.2.0

--- applications/update-application.php
+++ applications/update-application.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -7,45 +7,13 @@ $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client(new \Nexmo\Client\Credentials\Container($basic));
 
 try {
-    $application = $client->applications()->update([
-            'name' => 'Sample PHP V2 Application',
-            'capabilities' => [
-                'voice' => [
-                    'webhooks' => [
-                        'answer_url' => [
-                            'address' => "https://example.com/webhooks/answer",
-                            'http_method' => "GET"
-                        ],
-                        'event_url' => [
-                            'address' => "https://example.com/webhooks/event",
-                            'http_method' => "POST"
-                        ]
-                    ]
-                ],
-                'messages' => [
-                    'webhooks' => [
-                        'inbound_url' => [
-                            'address' => "https://example.com/webhooks/inbound",
-                            'http_method' => "POST"
-                        ],
-                        'status_url' => [
-                            'address' => "https://example.com/webhooks/status",
-                            'http_method' => "POST"
-                        ]
-                    ]
-                ],
-                'rtc' => [
-                    'webhooks' => [
-                        'event_url' => [
-                            'address' => "https://example.com/webhooks/rtcevent",
-                            'http_method' => "POST"
-                        ]
-                    ]
-                ]
-            ]
-        ],
-        MESSAGES_APPLICATION_ID
+    $application = $client->applications()->get(MESSAGES_APPLICATION_ID);
+    $application->setName('New Name2');
+    $application->getVoiceConfig()->setWebhook(
+        Nexmo\Application\VoiceConfig::ANSWER,
+        new Nexmo\Application\Webhook('http://test.domain/webhook/voice')
     );
+    $application = $client->applications()->update($application);
 
     echo $application->getId() . PHP_EOL;
     echo $application->getName() . PHP_EOL;
