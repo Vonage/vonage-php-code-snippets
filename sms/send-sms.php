8d37b8dca8d4dfa398de9eb2340cfdf23c0e3360
Chris Tankersley <chris@ctankersley.com>
2020-06-29 13:53:04 -0400

    Updated SMS sending with new SMS namespace

--- sms/send-sms.php
+++ sms/send-sms.php
@@ -1,24 +1,22 @@
 <?php
+
+use Zend\Diactoros\Response\Serializer;
+use Zend\Diactoros\Request\Serializer as asdf;
+
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client($basic);
 
-try {
-    $message = $client->message()->send([
-        'to' => TO_NUMBER,
-        'from' => 'Acme Inc',
-        'text' => 'A text message sent using the Nexmo SMS API'
-    ]);
-    $response = $message->getResponseData();
+$response = $client->sms()->send(
+    new \Nexmo\SMS\Message\SMS(TO_NUMBER, BRAND_NAME, 'A text message sent using the Nexmo SMS API')
+);
 
-    if($response['messages'][0]['status'] == 0) {
-        echo "The message was sent successfully\n";
-    } else {
-        echo "The message failed with status: " . $response['messages'][0]['status'] . "\n";
-    }
-} catch (Exception $e) {
-    echo "The message was not sent. Error: " . $e->getMessage() . "\n";
+$message = $response->current();
+
+if($message->getStatus() == 0) {
+    echo "The message was sent successfully\n";
+} else {
+    echo "The message failed with status: " . $message->getStatus() . "\n";
 }
-
--- sms/send-unicode-sms.php
+++ sms/send-unicode-sms.php
@@ -1,15 +1,13 @@
 <?php
+
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $basic  = new \Nexmo\Client\Credentials\Basic(NEXMO_API_KEY, NEXMO_API_SECRET);
 $client = new \Nexmo\Client($basic);
 
-$message = $client->message()->send([
-    'to' => TO_NUMBER,
-    'from' => 'Acme Inc',
-    'text' => 'こんにちは世界',
-    'type' => 'unicode'
-]);
+$response = $client->sms()->send(
+    new \Nexmo\SMS\Message\SMS(TO_NUMBER, BRAND_NAME, 'こんにちは世界')
+);
 
-var_dump($message->getResponseData());
+var_dump($response->current());
