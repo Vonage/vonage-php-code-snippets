082a58ca75e731aec0d7934cbb2c033a66584d09
gthoffer <gthoffer@gmail.com>
2020-10-02 15:22:16 -0500

    Fix #52 - cast 'NUMBER_SEARCH_CRITERIA' as (string).  Added comment to caution use of criteria starting with '0' to avoid conversion-to-octal issues.  Added some more comments while I was at it to help users understand the use of the filter object

--- numbers/search-available.php
+++ numbers/search-available.php
@@ -9,8 +9,17 @@ $basic = new \Vonage\Client\Credentials\Basic(VONAGE_API_KEY, VONAGE_API_SECRET)
 $client = new \Vonage\Client($basic);
 
 /** @var IterableAPICollection $response */
+/** Note: be careful when specifying "NUMBER_SEARCH_CRITERIA" environment variable: 
+  * surround it with quotes if it has a leading zero, otherwise the (string) conversion
+  * will assume it is an Octal number and convert it.  e.g., (string)0123 results in "83".
+  * NUMBER_SEARCH_PATTERN will be one of 0 (search for numbers that start with pattern);
+  * 1 (search for numbers that contain pattern), or 2 (search for numbers that end with pattern).
+  * VONAGE_NUMBER_TYPE must be one of: landline, mobile-lvn or landline-toll-free
+  * VONAGE_NUMBER_FEATURES Must be one of: SMS, VOICE, SMS,VOICE, MMS, SMS,MMS, VOICE,MMS 
+  *  or SMS,MMS,VOICE
+  */
 $filter = new AvailableNumbers([
-    "pattern" => (int) NUMBER_SEARCH_CRITERIA,
+    "pattern" => (string)NUMBER_SEARCH_CRITERIA,
     "search_pattern" => (int) NUMBER_SEARCH_PATTERN,
     "type" => VONAGE_NUMBER_TYPE,
     "features" => VONAGE_NUMBER_FEATURES,
