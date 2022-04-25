ef02f05839b926aa38f72fbd9896287048a76b1f
James Seconde <jim.seconde@googlemail.com>
2022-04-25 16:02:22 +0100

    missed namespace change, added pin details to verify

--- verify/request.php
+++ verify/request.php
@@ -7,6 +7,13 @@ $basic  = new \Vonage\Client\Credentials\Basic(VONAGE_API_KEY, VONAGE_API_SECRET
 $client = new \Vonage\Client(new \Vonage\Client\Credentials\Container($basic));
 
 $request = new \Vonage\Verify\Request(NUMBER, BRAND_NAME);
+
+// choose PIN length (4 or 6)
+$request->setCodeLength(4);
+
+// set locale
+$request->setCountry('de');
+
 $response = $client->verify()->start($request);
 
 echo "Started verification, `request_id` is " . $response->getRequestId();
