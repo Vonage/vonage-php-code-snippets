074f8984191766984ff05e91c9472bfc6c855327
James Seconde <jim.seconde@googlemail.com>
2022-06-15 16:27:22 +0100

    Update messages/viber/send-text.php
    Co-authored-by: Karl Lingiah <karl@superchilled.co.uk>

--- messages/viber/send-text.php
+++ messages/viber/send-text.php
@@ -9,7 +9,7 @@ $keypair = new \Vonage\Client\Credentials\Keypair(
 
 $client = new \Vonage\Client($keypair);
 
-$sms = new \Vonage\Messages\MessageType\Viber\ViberText(
+$viber = new \Vonage\Messages\MessageType\Viber\ViberText(
     TO_NUMBER,
     FROM_NUMBER,
     'This is a text message sent using the Vonage PHP SDK'
