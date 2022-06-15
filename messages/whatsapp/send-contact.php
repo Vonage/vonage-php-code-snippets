258046076b3ed01fb5d10715a09e16d983c8dd43
James Seconde <jim.seconde@googlemail.com>
2022-06-15 16:28:07 +0100

    Update messages/whatsapp/send-contact.php
    Co-authored-by: Karl Lingiah <karl@superchilled.co.uk>

--- messages/whatsapp/send-contact.php
+++ messages/whatsapp/send-contact.php
@@ -61,7 +61,7 @@ $custom = [
     ],
 ];
 
-$sms = new \Vonage\Messages\MessageType\WhatsApp\WhatsAppCustom(
+$whatsApp = new \Vonage\Messages\MessageType\WhatsApp\WhatsAppCustom(
     TO_NUMBER,
     FROM_NUMBER,
     $custom
