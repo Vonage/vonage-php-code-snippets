309c297b7b650cc02ef104b35428158a729fa468
James Seconde <jim.seconde@googlemail.com>
2023-05-03 11:09:00 +0100

    Add code snippets for checking an incoming message

---
+++ messages/signed-webhooks/verify-webhook.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '../../config.php';
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$signature = new Vonage\Client\Credentials\SignatureSecret(VONAGE_API_KEY, VONAGE_SIGNATURE_SECRET, 'sha256');
+$client = new Vonage\Client($signature);
+
+$message = new Vonage\SMS\Message\SMS(
+    TO_NUMBER,
+    FROM_NUMBER,
+    'This is a signed text'
+);
+
+$client->sms()->send($message);
+
+// Incoming Request
+$signature = new Vonage\Client\Credentials\SignatureSecret(VONAGE_API_KEY, VONAGE_SIGNATURE_SECRET, 'sha256');
+$isValid = $signature->check($_GET['sig']);
