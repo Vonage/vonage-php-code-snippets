f255108c5f4916868e5cedcae1c960b47480c975
James Seconde <jim.seconde@googlemail.com>
2023-05-18 13:55:03 +0100

    Change cancel input to constant

--- verify2/cancel.php
+++ verify2/cancel.php
@@ -7,5 +7,4 @@ $client = new Vonage\Client(
     new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
 );
 
-$requestId = 'c11236f4-00bf-4b89-84ba-88b25df97315';
-$client->verify2()->cancel($requestId);
+$client->verify2()->cancel(REQUEST_ID);
