07df9ea7b01e1bb4154a33419d7c51d2183e8851
James Seconde <jim.seconde@googlemail.com>
2023-05-24 10:52:10 +0100

    change to reflect the spec file

---
+++ verify2/send-request-email.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$client = new Vonage\Client(
+    new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
+);
+
+$newRequest = new \Vonage\Verify2\Request\EmailRequest(TO_EMAIL, 'my-verification', FROM_EMAIL);
+$client->verify2()->startVerification($newRequest);
---
+++ verify2/send-request-sms.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$client = new Vonage\Client(
+    new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
+);
+
+$newRequest = new \Vonage\Verify2\Request\SMSRequest(TO_NUMBER, 'my-verification');
+$client->verify2()->startVerification($newRequest);
