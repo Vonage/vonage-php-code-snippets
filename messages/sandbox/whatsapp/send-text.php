955695dd9701124678ed0c1b48765355447e92ac
superchilled <karl@superchilled.co.uk>
2024-04-03 16:38:56 +0100

    Adding Messages Sandbox snippets

---
+++ messages/sandbox/whatsapp/send-text.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '../../config.php';
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$keypair = new \Vonage\Client\Credentials\Keypair(
+    file_get_contents(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+    VONAGE_APPLICATION_ID
+);
+
+$client = new \Vonage\Client(
+    $keypair,
+    [
+        'base_api_url' => MESSAGES_SANDBOX_URL
+    ]   
+);
+
+$whatsApp = new \Vonage\Messages\MessageType\WhatsApp\WhatsAppText(
+    MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER,
+    MESSAGES_SANDBOX_WHATSAPP_NUMBER,
+    'This is a WhatsApp text message sent using the Vonage PHP SDK via the Messages Sandbox via the Messages Sandbox'
+);
