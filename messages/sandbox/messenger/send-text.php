e19dbc672aa9904dd2f0edd74eafff8334e38538
Karl Lingiah <karl@superchilled.co.uk>
2024-04-09 15:58:27 +0100

    Adding Messages Sandbox snippets (#98)
    * Adding MEssages Sandbox snippets
    
    * add send method
    
    * add send method
    
    * add send method
    
    ---------
    
    Co-authored-by: James Seconde <jim.seconde@googlemail.com>

---
+++ messages/sandbox/messenger/send-text.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '../../config.php';
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$keypair = new \Vonage\Client\Credentials\Keypair(
+    file_get_contents(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+    VONAGE_APPLICATION_ID
+);
+
+$client = new \Vonage\Client(
+    $keypair,
+    [
+        'base_api_url' => MESSAGES_SANDBOX_URL
+    ]   
+);
+
+$message = new \Vonage\Messages\MessageType\Messenger\MessengerText(
+    MESSAGES_SANDBOX_ALLOW_LISTED_FB_RECIPIENT_ID,
+    MESSAGES_SANDBOX_FB_ID,
+    'This is a Messenger text message sent using the Vonage PHP SDK via the Messages Sandbox'
+);
+
+$response = $client->messages()->send($message);
