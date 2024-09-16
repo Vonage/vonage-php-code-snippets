cfedca558a2c47f012b866807b6f049061158a08
Jim Seconde <jim.seconde@googlemail.com>
2024-09-16 11:15:28 +0100

    add custom template fragments

---
+++ verify2/create-template-fragment.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$client = new Vonage\Client(
+    new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
+);
+
+$createFragmentRequest = new CreateCustomTemplateFragmentRequest(
+    'sms',
+    'en-us',
+    'The authentication code for your ${brand} is: ${code}'
+);
+
+$client->verify2()->createCustomTemplateFragment(TEMPLATE_ID, $createFragmentRequest);
---
+++ verify2/update-template.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$client = new Vonage\Client(
+    new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
+);
+
+$updateRequest = new UpdateCustomTemplateRequest('my-renamed-template', true);
+
+$client->verify2()->updateCustomTemplate(TEMPLATE_ID, $updateRequest);
