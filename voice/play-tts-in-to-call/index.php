aa50cc026f38828d1fddef0441a58b47d46e8eaa
Jim Seconde <jim.seconde@googlemail.com>
2024-09-19 16:53:48 +0100

    add language to talk action

--- voice/play-tts-in-to-call/index.php
+++ voice/play-tts-in-to-call/index.php
@@ -4,8 +4,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $keypair = new \Vonage\Client\Credentials\Keypair(file_get_contents(VONAGE_APPLICATION_PRIVATE_KEY_PATH), VONAGE_APPLICATION_ID);
 $client = new \Vonage\Client($keypair);
+$action = new \Vonage\Voice\NCCO\Action\Talk(TEXT);
+$action->setLanguage('en-US');
 
 $client->voice()->playTTS(
     UUID,
-    new \Vonage\Voice\NCCO\Action\Talk(TEXT)
+    $action
 );
