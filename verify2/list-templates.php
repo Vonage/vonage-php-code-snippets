1a6048d44aa61af3a01ca8e71a19976eb6a363c5
Jim Seconde <jim.seconde@googlemail.com>
2024-10-15 09:59:00 +0100

    Added lists

---
+++ verify2/list-templates.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$client = new Vonage\Client(
+    new Vonage\Client\Credentials\Keypair(VONAGE_APPLICATION_PRIVATE_KEY_PATH, VONAGE_APPLICATION_ID),
+);
+
+$template = $client->verify2()->listCustomTemplates(TEMPLATE_ID);
