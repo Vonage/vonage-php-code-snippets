8b904d3f1d328b0bde4d75804a305ee6724c7835
Jim Seconde <jim.seconde@googlemail.com>
2024-12-10 11:52:27 +0000

    fix balance transfer object

--- subaccounts/transfer-credit.php
+++ subaccounts/transfer-credit.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $basic = new \Vonage\Client\Credentials\Basic(VONAGE_API_KEY, VONAGE_API_SECRET);
 $client = new \Vonage\Client($basic);
 
-$transferRequest = new \Vonage\Subaccount\Request\TransferCreditRequest(VONAGE_API_KEY);
+$transferRequest = new \Vonage\Subaccount\Request\TransferBalanceRequest(VONAGE_API_KEY);
 $transferRequest
     ->setFrom(VONAGE_API_KEY)
     ->setTo(SUBACCOUNT_KEY)
