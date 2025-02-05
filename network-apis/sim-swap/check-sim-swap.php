ac0e918ec2dacfe3c49c07fc9ca6222760e6d6f2
Jim Seconde <jim.seconde@googlemail.com>
2025-02-05 10:53:55 +0000

    Add GNP snippets

---
+++ network-apis/sim-swap/check-sim-swap.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '../../config.php';
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$credentials = new \Vonage\Client\Credentials\Gnp(
+    VONAGE_PHONE_NUMBER,
+    file_get_contents(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+    VONAGE_APPLICATION_ID
+);
+
+$client = new \Vonage\Client($credentials);
+
+$result = $client->simswap()->checkSimSwap(PHONE_NUMBER);
