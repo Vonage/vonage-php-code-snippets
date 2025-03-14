21e4fa13c22b96bd4b05688bd81529e00abf4d9f
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-03-14 09:52:28 -0400

    refactor: updated credentials

--- sms/send-sms.php
+++ sms/send-sms.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container  = new \Vonage\Client\Credentials\Container(VONAGE_APPLICATION_ID, VONAGE_PRIVATE_KEY);
-$client = new \Vonage\Client($container);
+$keypair = new \Vonage\Client\Credentials\Keypair(VONAGE_PRIVATE_KEY, VONAGE_APPLICATION_ID);
+$client = new \Vonage\Client($keypair);
 
 $response = $client->sms()->send(
     new \Vonage\SMS\Message\SMS(TO_NUMBER, BRAND_NAME, 'A text message sent using the Vonage SMS API')
